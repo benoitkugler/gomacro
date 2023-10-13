13468955296948a57317444f5ec7f84c3b6fbad3
Benoit KUGLER <benoit.kugler@gmail.com>
2023-10-13 10:11:06 +0200

    support for int16

--- generator/go/randdata/data.go
+++ generator/go/randdata/data.go
@@ -124,6 +124,8 @@ func (ctx context) codeForBasic(bs *an.Basic) gen.Declaration {
 		code = fnInt("uint8")
 	case types.Int8:
 		code = fnInt("int8")
+	case types.Int16:
+		code = fnInt("int16")
 	case types.Float64:
 		code = fnFloat64()
 	case types.String:
